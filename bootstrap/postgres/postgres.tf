43fae681352c90e3f1583e35de3aa375d05e81fe
gonzalezzfelipe <gonzalezz_felipe@hotmail.com>
2024-04-23 17:01:53 -0300

    Change to x86

--- bootstrap/postgres/postgres.tf
+++ bootstrap/postgres/postgres.tf
@@ -40,13 +40,13 @@ resource "kubernetes_manifest" "postgres" {
           "effect"   = "NoSchedule"
           "key"      = "demeter.run/compute-arch"
           "operator" = "Equal"
-          "value"    = "arm64"
+          "value"    = "x86"
         },
         {
           "effect"   = "NoSchedule"
           "key"      = "demeter.run/availability-sla"
           "operator" = "Equal"
-          "value"    = "consistent"
+          "value"    = "best-effort"
         }
       ]
       "serviceAnnotations" : {
